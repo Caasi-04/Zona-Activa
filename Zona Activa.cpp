dfa063b7ea641b0029b1d486795b0b8e2b609fe8
Caasi-04 <ih2987577@gmail.com>
2026-08-17 14:29:13 -0600

    Add comment about file purpose
    Added a comment indicating the file's purpose.

--- Zona Activa.cpp
+++ Zona Activa.cpp
@@ -1,3 +1,5 @@
+//Este archivo no hace absolutamente nada, pero si no está no funciona, lo mejor es no tocarlo
+
 // Zona Activa.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
